71890f5059aaab21c2973bc53305945e94f4b584
danbi <4679jeong@naver.com>
2024-06-23 23:19:36 +0900

    first

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Sec1 from "./pages/sec1"
+
+function App() {
+ 
+
+  return (
+    <>
+    <Sec1/>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/sec1.jsx
@@ -0,0 +1,17 @@
+import './style.css'
+
+const Sec1 = () => {
+
+    return(
+        <div className="basic_section sec1">
+        <h2><span>D</span>elight</h2>
+        <h2><span>A</span>wesome</h2>
+        <h2><span>N</span>avigate</h2>
+        <h2><span>B</span>enevolence</h2>
+        <h2><span>I</span>deaful</h2>
+        <h3 className='last_name'>DANBI</h3>
+        </div>
+    )
+}
+
+export default Sec1
