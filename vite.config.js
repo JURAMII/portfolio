dbd998139d12e4ccaa712d968af8d0dcf9d68d9d
juram <4679jeong@naver.com>
2024-08-12 12:45:30 +0900

    0812 pwa

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,36 @@ import { VitePWA } from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/portfolio/",
-  plugins: [react(), VitePWA()],
+  plugins: [react(), 
+  VitePWA(
+    {
+      registerType: 'autoUpdate',
+      injectRegister: false,
+  
+      pwaAssets: {
+        disabled: false,
+        config: true,
+      },
+  
+      manifest: {
+        name: "DANBI's portfolio",
+        short_name: "DANBI's portfolio", //아이콘바로가기이름
+        description: 'DANBI',
+        theme_color: '#ffffff',
+      },
+  
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+        cleanupOutdatedCaches: true,
+        clientsClaim: true,
+      },
+  
+      devOptions: {
+        enabled: true,
+        navigateFallback: 'index.html',
+        suppressWarnings: true,
+        type: 'module',
+      },
+    }
+  )],
 })
